ed9f7e6f7c739ff0a0db96133860c9f2f764f6da
LuoXiande123 <1749733965@qq.com>
2021-03-21 17:42:15 +0800

    HW2-CE1 Commit

--- src/main/java/com/example/LuoXiande/Week2/HelloWorldServlet.java
+++ src/main/java/com/example/LuoXiande/Week2/HelloWorldServlet.java
@@ -1,15 +1,18 @@
-package com.example.LuoXiande;
+package com.example.LuoXiande.Week2;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 public class HelloWorldServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
-        writer.println("hello cilent !!!");
+        writer.println("Name : LuoXiande");
+        writer.println("Id : 2019211001000719");
+        writer.println("Date and Time " + new Date());
     }
     public void doPost(HttpServletRequest request,HttpServletResponse response){
 
