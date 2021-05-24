7751d91fc923163da7eb851699c68bdd4d0e376d
LuoXiande123 <1749733965@qq.com>
2021-05-24 18:53:13 +0800

    HW 12

--- src/main/java/com/LuoXiande/model/Category.java
+++ src/main/java/com/LuoXiande/model/Category.java
@@ -8,62 +8,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Category {
-    private int CategoryId;
-    private String CategoryName;
-    private String Description;
-    private Boolean Active;
+    private int categoryId;
+    private String categoryName;
+    private String description;
+    private Boolean active;
 
     public  Category(){}
 
 
-    public Category(int categoryId, String categoryName, String description, Boolean active) {
-        CategoryId = categoryId;
-        CategoryName = categoryName;
-        Description = description;
-        Active = active;
-    }
-
     public int getCategoryId() {
-        return CategoryId;
+        return categoryId;
     }
 
     public void setCategoryId(int categoryId) {
-        CategoryId = categoryId;
+        this.categoryId = categoryId;
     }
 
     public String getCategoryName() {
-        return CategoryName;
+        return categoryName;
     }
 
     public void setCategoryName(String categoryName) {
-        CategoryName = categoryName;
+        this.categoryName = categoryName;
     }
 
     public String getDescription() {
-        return Description;
+        return description;
     }
 
     public void setDescription(String description) {
-        Description = description;
+        this.description = description;
     }
 
     public Boolean getActive() {
-        return Active;
+        return active;
     }
 
     public void setActive(Boolean active) {
-        Active = active;
+        this.active = active;
     }
 
     @Override
     public String toString() {
         return "Category{" +
-                "CategoryId=" + CategoryId +
-                ", CategoryName='" + CategoryName + '\'' +
-                ", Description='" + Description + '\'' +
-                ", Active=" + Active +
+                "categoryId=" + categoryId +
+                ", categoryName='" + categoryName + '\'' +
+                ", description='" + description + '\'' +
+                ", active=" + active +
                 '}';
     }
+
     public static List<Category> findAllCategory(Connection con){
         List<Category> list=new ArrayList<Category>();
         String queryString="select * from Category";
@@ -72,10 +66,10 @@ public class Category {
             ResultSet resultSet=statement.executeQuery();
             while(resultSet.next()){
                 Category category=new Category();
-                category.setCategoryId(resultSet.getInt("CategoryId"));
+                category.setCategoryId(resultSet.getInt("categoryId"));
                 category.setCategoryName(resultSet.getNString("CategoryName"));
-                category.setDescription(resultSet.getNString("Description"));
-                category.setActive(resultSet.getBoolean("Active"));
+                category.setDescription(resultSet.getNString("description"));
+                category.setActive(resultSet.getBoolean("active"));
                 list.add(category);
             }
         } catch (SQLException throwables) {
